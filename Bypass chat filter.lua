9539406d7353716752d0510f495207f58e1c63ec
Esdeath <39652390+notesdeath@users.noreply.github.com>
2025-01-08 19:53:23 +0100

    Update Bypass chat filter.lua

--- Bypass chat filter.lua
+++ Bypass chat filter.lua
@@ -1,145 +1,185 @@
-﻿local var_0_0 = require("ffi")
-local var_0_1 = require("gamesense/steamworks")
-local var_0_2 = var_0_0.typeof("char[?]")
-local var_0_3 = vtable_bind("steamclient.dll", "SteamClient017", 9, "uintptr_t*(__thiscall*)(void*, int, const char*)")
-local var_0_4 = vtable_thunk(32, "int(__thiscall*)(void*, char*, uint32_t, const char*, bool)")
-local var_0_5 = var_0_3(1, "SteamUtils009")
+local ffi = require "ffi"
+local zeroWidthChar = '\u{200C}​'
 
-if var_0_5 == nil then
+local function vmt_entry(instance, index, type)
+	return ffi.cast(type, (ffi.cast("void***", instance)[0])[index])
+end
+
+local function vmt_thunk(index, typestring)
+	local t = ffi.typeof(typestring)
+	return function(instance, ...)
+		assert(instance ~= nil)
+		if instance then
+			return vmt_entry(instance, index, t)(instance, ...)
+		end
+	end
+end
+
+local function vmt_bind(module, interface, index, typestring)
+	local instance = client.create_interface(module, interface) or error("invalid interface")
+	local fnptr = vmt_entry(instance, index, ffi.typeof(typestring)) or error("invalid vtable")
+	return function(...)
+		return fnptr(instance, ...)
+	end
+end
+
+local new_charbuf = ffi.typeof("char[?]")
+
+local native_GetISteamUtils = vmt_bind("steamclient.dll", "SteamClient017", 9, "uintptr_t*(__thiscall*)(void*, int, const char*)")
+local native_ISteamUtils_FilterText = vmt_thunk(32, "int(__thiscall*)(void*, char*, uint32_t, const char*, bool)")
+
+local ISteamUtils = native_GetISteamUtils(1, "SteamUtils009")
+
+if ISteamUtils == nil then
 	return error("failed to get ISteamUtils")
 end
 
-local function var_0_6(arg_1_0, arg_1_1)
-	local var_1_0 = arg_1_0:len() + 1
-	local var_1_1 = var_0_2(var_1_0)
-	local var_1_2 = var_0_4(var_0_5, var_1_1, var_1_0, arg_1_0, arg_1_1)
+local function filter_text(text, legal_only)
+	local size = text:len() + 1
+	local pchOutFilteredText = new_charbuf(size)
+	local nCharsFiltered = native_ISteamUtils_FilterText(ISteamUtils, pchOutFilteredText, size, text, legal_only)
 
-	return var_0_0.string(var_1_1, var_1_0 - 1), var_1_2
+	return ffi.string(pchOutFilteredText, size-1), nCharsFiltered
 end
 
-local var_0_7 = ui.new_checkbox("LUA", "B", "Bypass Chat Filter")
-local var_0_8 = {
-	i = "į",
-	y = "ỵ",
-	H = "Ḥ",
-	X = "Ẋ",
-	h = "ḥ",
-	x = "ẋ",
-	G = "Ġ",
-	W = "Ẉ",
-	g = "ġ",
-	w = "ẉ",
-	F = "Ḟ",
-	V = "Ṿ",
-	f = "ḟ",
-	v = "ṿ",
-	E = "Ẹ",
-	U = "Ụ",
-	e = "ẹ",
-	u = "ụ",
-	D = "Ḍ",
-	T = "Ṭ",
-	d = "ḍ",
-	t = "ṭ",
-	C = "Ċ",
-	S = "Ṣ",
-	c = "ċ",
-	s = "ṣ",
-	R = "Ṛ",
-	b = "ḃ",
-	r = "ṛ",
-	A = "Ȧ",
-	Q = "Ɋ",
-	a = "ȧ",
-	n = "ṇ",
-	q = "ɋ",
-	B = "Ḃ",
-	P = "Ṗ",
-	p = "ṗ",
-	O = "Ȯ",
-	o = "ȯ",
-	N = "Ṇ",
-	M = "Ṃ",
-	m = "ṃ",
-	L = "Ḷ",
-	l = "ḷ",
-	K = "Ḳ",
-	k = "ḳ",
-	J = "Ĵ",
-	Z = "Ẓ",
-	j = "ĵ",
-	z = "ẓ",
-	I = "Į",
-	Y = "Ỵ"
-}
-local var_0_9 = "‌​"
-local var_0_10 = " "
-local var_0_11 = {
-	MyPersonaAPI = panorama.open().MyPersonaAPI,
-	PartyListAPI = panorama.open().PartyListAPI
+local bypassFilter = ui.new_checkbox('LUA', 'B', 'Bypass Chat Filter')
+
+local alphabet = {
+	['a'] = 'ȧ',
+	['A'] = 'Ȧ',
+	['b'] = 'ḃ',
+	['B'] = 'Ḃ',
+	['c'] = 'ċ',
+	['C'] = 'Ċ',
+	['d'] = 'ḍ',
+	['D'] = 'Ḍ',
+	['e'] = 'ẹ',
+	['E'] = 'Ẹ',
+	['f'] = 'ḟ',
+	['F'] = 'Ḟ',
+	['g'] = 'ġ',
+	['G'] = 'Ġ',
+	['h'] = 'ḥ',
+	['H'] = 'Ḥ',
+	['i'] = 'į',
+	['I'] = 'Į',
+	['j'] = 'ĵ',
+	['J'] = 'Ĵ',
+	['k'] = 'ḳ',
+	['K'] = 'Ḳ',
+	['l'] = 'ḷ',
+	['L'] = 'Ḷ',
+	['m'] = 'ṃ',
+	['M'] = 'Ṃ',
+	['n'] = 'ṇ',
+	['N'] = 'Ṇ',
+	['o'] = 'ȯ',
+	['O'] = 'Ȯ',
+	['p'] = 'ṗ',
+	['P'] = 'Ṗ',
+	['q'] = 'ɋ',
+	['Q'] = 'Ɋ',
+	['r'] = 'ṛ',
+	['R'] = 'Ṛ',
+	['s'] = 'ṣ',
+	['S'] = 'Ṣ',
+	['t'] = 'ṭ',
+	['T'] = 'Ṭ',
+	['u'] = 'ụ',
+	['U'] = 'Ụ',
+	['v'] = 'ṿ',
+	['V'] = 'Ṿ',
+	['w'] = 'ẉ',
+	['W'] = 'Ẉ',
+	['x'] = 'ẋ',
+	['X'] = 'Ẋ',
+	['y'] = 'ỵ',
+	['Y'] = 'Ỵ',
+	['z'] = 'ẓ',
+	['Z'] = 'Ẓ',
 }
 
-local function var_0_12(arg_2_0)
-	local var_2_0 = {}
+local cache = {
+	open = panorama.open,
+	format = string.format
+}
 
-	for iter_2_0 in arg_2_0:gmatch("[^%s]+") do
-		local var_2_1, var_2_2 = var_0_6(iter_2_0, false)
+local Panorama = {
+	MyPersonaAPI = cache.open().MyPersonaAPI,
+	PartyListAPI = cache.open().PartyListAPI
+}
 
-		if var_2_2 > 0 and iter_2_0:len() > 1 then
-			local var_2_3 = iter_2_0:sub(3, 3)
+local NoBreakSpace = '\u{00A0}'
 
-			var_2_3 = var_0_8[var_2_3] or var_2_3
-			iter_2_0 = iter_2_0:sub(1, 2) .. var_2_3 .. iter_2_0:sub(4, -1)
-		end
+local StringExplode = function(separator, str)
+	local ret = {}
+	local currentPos = 1
 
-		table.insert(var_2_0, iter_2_0)
+	for i = 1, #str do
+		local startPos, endPos = string.find(str, separator, currentPos)
+		if ( not startPos ) then break end
+		ret[ i ] = string.sub( str, currentPos, startPos - 1 )
+		currentPos = endPos + 1
 	end
 
-	var_0_11.PartyListAPI.SessionCommand("Game::Chat", string.format("run all xuid %s chat %s", var_0_11.MyPersonaAPI.GetXuid(), table.concat(var_2_0, var_0_10)))
+	ret[#ret + 1] = string.sub( str, currentPos )
+
+	return ret
 end
 
-client.set_event_callback("console_input", function(arg_3_0)
-	if arg_3_0:sub(1, #"party_say") == "party_say" then
-		arg_3_0 = arg_3_0:sub(#"party_say" + 2, -1)
+local PartyChatSay = function(text)
+	local newMessage = {}
 
-		var_0_12(arg_3_0)
+	for word in text:gmatch('[^%s]+') do
+		local text_filtered, characters_filtered = filter_text(word, false)
 
+		if (characters_filtered > 0 and word:len() > 1) then
+			local letter = word:sub(3, 3)
+			letter = alphabet[letter] or letter
+
+			word = word:sub(1, 2) .. letter .. word:sub(4, -1)
+		end
+
+		table.insert(newMessage, word)
+	end
+
+	Panorama.PartyListAPI.SessionCommand('Game::Chat', cache.format('run all xuid %s chat %s', Panorama.MyPersonaAPI.GetXuid(), table.concat(newMessage, NoBreakSpace)))
+end
+
+client.set_event_callback('console_input', function(text)
+	if (text:sub(1, #'party_say') == 'party_say') then
+		text = text:sub(#'party_say' + 2, -1)
+
+		PartyChatSay(text)
 		return true
 	end
 end)
-client.set_event_callback("string_cmd", function(arg_4_0)
-	if not ui.get(var_0_7) then
-		return
-	end
 
-	local var_4_0, var_4_1 = arg_4_0.text:match("^(.-) (.+)$")
+client.set_event_callback('string_cmd', function(e)
+	local text = e.text
+	local command, message = text:match('^(.-) (.*)$')
 
-	if (var_4_0 == "say" or var_4_0 == "say_team") and var_4_1 ~= nil then
-		local var_4_2 = false
-		local var_4_3 = {}
+	if ((command == 'say' or command == 'say_team') and message ~= nil and message ~= '') then
+		local newMessage = {}
 
-		if var_4_1:find("\"", 1) and var_4_1:find("\"", -1) then
-			var_4_1 = var_4_1:sub(2, -2)
+		if (message:find('"', 1) and message:find('"', -1)) then
+			message = message:sub(2, -2)
 		end
 
-		for iter_4_0 in var_4_1:gmatch("[^%s]+") do
-			local var_4_4, var_4_5 = var_0_6(iter_4_0, false)
+		for word in message:gmatch('[^%s]+') do
+			local text_filtered, characters_filtered = filter_text(word, false)
 
-			if var_4_5 > 0 and iter_4_0:len() > 1 then
-				var_4_2 = true
+			if (ui.get(bypassFilter) and characters_filtered > 0 and word:len() > 1) then
+				local letterCount = word:len() > 2 and 3 or 2
+				local letter = word:sub(letterCount, letterCount)
+				letter = alphabet[letter] or letter
 
-				local var_4_6 = iter_4_0:len() > 2 and 3 or 2
-				local var_4_7 = iter_4_0:sub(var_4_6, var_4_6)
-
-				var_4_7 = var_0_8[var_4_7] or var_4_7
-				iter_4_0 = iter_4_0:sub(1, var_4_6 - 1) .. var_4_7 .. iter_4_0:sub(var_4_6 + 1, -1)
+				word = word:sub(1, letterCount - 1) .. letter .. word:sub(letterCount + 1, -1)
 			end
 
-			table.insert(var_4_3, iter_4_0)
+			table.insert(newMessage, word)
 		end
 
-		if var_4_2 then
-			arg_4_0.text = var_4_0 .. " " .. table.concat(var_4_3, " ")
-		end
+        e.text = command .. ' ' .. table.concat(newMessage, " ")
 	end
 end)
