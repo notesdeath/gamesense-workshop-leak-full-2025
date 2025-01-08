cd8735a331cca3c5328a3989b3e5eb33828cf6a8
Esdeath <39652390+notesdeath@users.noreply.github.com>
2025-01-08 21:54:10 +0100

    Update VALORANT kill effects.lua

--- VALORANT kill effects.lua
+++ VALORANT kill effects.lua
@@ -1,52 +1,77 @@
-﻿local var_0_0 = globals.realtime
-local var_0_1 = globals.mapname
-local var_0_2 = require("ffi")
-local var_0_3 = require("gamesense/gif_decoder")
-local var_0_4 = require("gamesense/http")
-local var_0_5 = {
-	surface = {
+-- VALORANT kill effects for Counter-Strike: Global Offensive, by nicole
+
+-- Localized functions
+local globals_realtime, globals_mapname = globals.realtime, globals.mapname
+
+-- Libraries
+local ffi = require "ffi"
+local gif_decoder = require "gamesense/gif_decoder"
+local http = require "gamesense/http"
+
+-- Game definitions
+local vftable_e =
+{
+	surface =
+	{
 		playsound = 82
 	}
 }
-local var_0_6 = {
+
+-- General enums
+local http_status_code_e =
+{
 	ok = 200
 }
-local var_0_7 = vtable_bind("vguimatsurface.dll", "VGUI_Surface031", var_0_5.surface.playsound, "void (__thiscall*)(void* ecx, const char* sPath)")
-local var_0_8 = ui.new_checkbox("VISUALS", "Effects", "VALORANT kill effects")
-local var_0_9 = "https://aether.moe/gamesense"
-local var_0_10 = 6
-local var_0_11 = 0
-local var_0_12 = 0
-local var_0_13 = 0
-local var_0_14 = 0
-local var_0_15
-local var_0_16
-local var_0_17
 
-function on_player_death(arg_1_0)
-	local var_1_0 = entity.get_local_player()
+-- FFI
+-- https://github.com/ValveSoftware/source-sdk-2013/blob/master/mp/src/public/vgui/ISurface.h#L272
+local ISurface__PlaySound = vtable_bind("vguimatsurface.dll", "VGUI_Surface031", vftable_e.surface.playsound, "void (__thiscall*)(void* ecx, const char* sPath)")
 
-	if client.userid_to_entindex(arg_1_0.attacker) ~= var_1_0 or arg_1_0.attacker == arg_1_0.userid or var_0_12 >= var_0_10 then
+-- Menu
+local g_pMenuKillEffectsEnabled = ui.new_checkbox("VISUALS", "Effects", "VALORANT kill effects")
+
+-- Constants
+local ASSET_CDN = "https://aether.moe/gamesense"
+local MAX_KILLS = 6
+
+-- Globals
+local g_nMissingFiles = 0
+local g_nKills = 0
+local g_flPlaybackStart = 0.0
+local g_flPlayUntil = 0.0
+local g_aBanners = nil
+local g_anDisplayAtX = nil
+local g_anDisplayAtY = nil
+
+function on_player_death(e)
+	local nLocalPlayer = entity.get_local_player()
+	
+	if client.userid_to_entindex(e.attacker) ~= nLocalPlayer or e.attacker == e.userid or g_nKills >= MAX_KILLS then
 		return
 	end
 
-	var_0_12 = var_0_12 + 1
+	g_nKills = g_nKills + 1
 
-	var_0_7(string.format("valorant_%d.wav", var_0_12))
+	-- Plays the sound
+	ISurface__PlaySound(string.format("valorant_%d.wav", g_nKills))
 
-	var_0_13 = globals.realtime()
-	var_0_14 = var_0_13 + var_0_15[var_0_12].duration
-	var_0_16, var_0_17 = client.screen_size()
-	var_0_16 = var_0_16 / 2
-	var_0_16 = var_0_16 - var_0_15[var_0_12].width / 2
-	var_0_17 = var_0_17 * 0.9
-	var_0_17 = var_0_17 - var_0_15[var_0_12].height
+	-- Displays the kill banner
+	g_flPlaybackStart = globals.realtime()
+	g_flPlayUntil = g_flPlaybackStart + g_aBanners[g_nKills].duration
+
+	-- Specify where to play the kill banner
+	g_anDisplayAtX, g_anDisplayAtY = client.screen_size()
+	g_anDisplayAtX = g_anDisplayAtX / 2 -- Center
+	g_anDisplayAtX = g_anDisplayAtX - g_aBanners[g_nKills].width / 2
+
+	g_anDisplayAtY = g_anDisplayAtY * 0.9 -- 90%-ish of screen height
+	g_anDisplayAtY = g_anDisplayAtY - g_aBanners[g_nKills].height
 end
 
-function on_player_spawn(arg_2_0)
-	local var_2_0 = entity.get_local_player()
-
-	if client.userid_to_entindex(arg_2_0.userid) ~= var_2_0 then
+function on_player_spawn(e)
+	local nLocalPlayer = entity.get_local_player()
+	
+	if client.userid_to_entindex(e.userid) ~= nLocalPlayer then
 		return
 	end
 
@@ -54,33 +79,30 @@ function on_player_spawn(arg_2_0)
 end
 
 function on_paint_ui()
-	local var_3_0 = var_0_0()
+	local flRealTime = globals_realtime()
 
-	if var_3_0 < var_0_14 and var_0_1() ~= nil then
-		local var_3_1 = var_0_15[var_0_12]
-
-		var_3_1:draw(var_3_0 - var_0_13, var_0_16, var_0_17, var_3_1.width, var_3_1.height, 255, 255, 255, 255)
+	if flRealTime < g_flPlayUntil and globals_mapname() ~= nil then
+		local pBanner = g_aBanners[g_nKills]
+		pBanner:draw(flRealTime - g_flPlaybackStart, g_anDisplayAtX, g_anDisplayAtY, pBanner.width, pBanner.height, 255, 255, 255, 255)
 	end
 end
 
 function reset_vars()
-	var_0_12 = 0
-	var_0_13 = 0
-	var_0_14 = 0
+	g_nKills = 0
+	g_flPlaybackStart = 0.0
+	g_flPlayUntil = 0.0
 end
 
-function set_callbacks(arg_5_0)
-	local var_5_0 = arg_5_0 and client.set_event_callback or client.unset_event_callback
+function set_callbacks(enable)
+	local pFunc = enable and client.set_event_callback or client.unset_event_callback
+	pFunc("player_death", on_player_death)
+	pFunc("player_spawn", on_player_spawn)
+	pFunc("paint_ui", on_paint_ui)
 
-	var_5_0("player_death", on_player_death)
-	var_5_0("player_spawn", on_player_spawn)
-	var_5_0("paint_ui", on_paint_ui)
-
-	if arg_5_0 and var_0_15 == nil then
+	if enable and g_aBanners == nil then
 		if not load_files() then
-			var_0_15 = nil
-
-			ui.set(var_0_8, false)
+			g_aBanners = nil
+			ui.set(g_pMenuKillEffectsEnabled, false)
 			client.error_log("Some assets could not be found. Attempting to retrieve them...")
 		else
 			client.log("VALORANT assets loaded.")
@@ -88,55 +110,53 @@ function set_callbacks(arg_5_0)
 	end
 end
 
-function on_menu_kill_effects_enabled_callback(arg_6_0)
-	set_callbacks(ui.get(arg_6_0))
+function on_menu_kill_effects_enabled_callback(ref)
+	set_callbacks(ui.get(ref))
 end
 
-function download_file(arg_7_0)
-	var_0_4.get(string.format("%s/%s", var_0_9, arg_7_0), function(arg_8_0, arg_8_1)
-		if not arg_8_0 or arg_8_1.status ~= var_0_6.ok then
-			client.error_log(string.format("Could not retrieve asset \"%s\" from server. Error code: %d", arg_7_0, arg_8_1.status))
-
+function download_file(path)
+	http.get(string.format("%s/%s", ASSET_CDN, path), function(success, response)
+		if not success or response.status ~= http_status_code_e.ok then
+			client.error_log(string.format("Could not retrieve asset \"%s\" from server. Error code: %d", path, response.status))
+	
 			return
 		end
+	
+		writefile(path, response.body)
+		g_nMissingFiles = g_nMissingFiles - 1
+		client.log(string.format("Successfully installed asset at path \"%s\".", path))
 
-		writefile(arg_7_0, arg_8_1.body)
-
-		var_0_11 = var_0_11 - 1
-
-		client.log(string.format("Successfully installed asset at path \"%s\".", arg_7_0))
-
-		if var_0_11 == 0 then
+		-- Finished downloading everything, re-enable the checkbox for a smooth first-time installation..
+		if g_nMissingFiles == 0 then
 			client.log("VALORANT assets downloaded. Loading them.. this might take a while.")
-			client.delay_call(globals.tickinterval(), ui.set, var_0_8, true)
+			client.delay_call(globals.tickinterval(), ui.set, g_pMenuKillEffectsEnabled, true)
 		end
 	end)
 end
 
-function check_and_download(arg_9_0)
-	if not readfile(arg_9_0) then
-		var_0_11 = var_0_11 + 1
-
-		download_file(arg_9_0)
+function check_and_download(path)
+	if not readfile(path) then
+		g_nMissingFiles = g_nMissingFiles + 1
+		download_file(path)
 	end
 end
 
 function load_files()
-	var_0_15 = {}
-	var_0_11 = 0
+	g_aBanners = {}
+	g_nMissingFiles = 0
 
-	for iter_10_0 = 1, var_0_10 do
-		check_and_download(string.format("csgo/sound/valorant_%d.wav", iter_10_0))
-		check_and_download(string.format("csgo/materials/valorant_%d.gif", iter_10_0))
+	for i = 1, MAX_KILLS do
+		check_and_download(string.format("csgo/sound/valorant_%d.wav", i))
+		check_and_download(string.format("csgo/materials/valorant_%d.gif", i))
 	end
 
-	if var_0_11 == 0 then
-		for iter_10_1 = 1, var_0_10 do
-			var_0_15[iter_10_1] = var_0_3.load_gif(readfile(string.format("csgo/materials/valorant_%d.gif", iter_10_1)))
+	if g_nMissingFiles == 0 then
+		for i = 1, MAX_KILLS do
+			g_aBanners[i] = gif_decoder.load_gif(readfile(string.format("csgo/materials/valorant_%d.gif", i)))
 		end
 	end
 
-	return var_0_11 == 0
+	return g_nMissingFiles == 0
 end
 
-ui.set_callback(var_0_8, on_menu_kill_effects_enabled_callback)
+ui.set_callback(g_pMenuKillEffectsEnabled, on_menu_kill_effects_enabled_callback)
