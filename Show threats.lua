d2c2289fe7ca1dd12f622544a1c4be4fa174b68f
Esdeath <39652390+notesdeath@users.noreply.github.com>
2025-01-09 15:03:46 +0100

    Update Show threats.lua

--- Show threats.lua
+++ Show threats.lua
@@ -1,104 +1,87 @@
-﻿local var_0_0 = entity.get_prop
-local var_0_1 = ui.get
-local var_0_2 = math.sqrt
-local var_0_3 = math.sin
-local var_0_4 = math.cos
-local var_0_5 = entity.get_local_player
-local var_0_6 = entity.get_players
-local var_0_7 = entity.get_player_name
-local var_0_8 = client.screen_size
-local var_0_9 = renderer.text
-local var_0_10 = math.rad
-local var_0_11 = var_0_4(var_0_10(10))
-local var_0_12 = ui.new_checkbox("VISUALS", "Other ESP", "Show threats")
+local get_prop         = entity.get_prop
+local ui_get           = ui.get
+local sqrt, sin, cos   = math.sqrt, math.sin, math.cos
+local get_local_player = entity.get_local_player
+local get_all_players  = entity.get_players
+local get_player_name  = entity.get_player_name
+local get_screen_size  = client.screen_size
+local text             = renderer.text
+local deg2rad          = math.rad
 
-local function var_0_13(arg_1_0, arg_1_1, arg_1_2)
-	local var_1_0 = var_0_2(arg_1_0 * arg_1_0 + arg_1_1 * arg_1_1 + arg_1_2 * arg_1_2)
+local fov_cos      = cos(deg2rad(10))
+local show_threats = ui.new_checkbox("VISUALS", "Other ESP", "Show threats")
 
-	if var_1_0 == 0 then
+local function vec3_normalize(x, y, z)
+	local len = sqrt(x * x + y * y + z * z)
+	if len == 0 then
 		return 0, 0, 0
 	end
-
-	local var_1_1 = 1 / var_1_0
-
-	return arg_1_0 * var_1_1, arg_1_1 * var_1_1, arg_1_2 * var_1_1
+	local r = 1 / len
+	return x*r, y*r, z*r
 end
 
-local function var_0_14(arg_2_0, arg_2_1, arg_2_2, arg_2_3, arg_2_4, arg_2_5)
-	return arg_2_0 * arg_2_3 + arg_2_1 * arg_2_4 + arg_2_2 * arg_2_5
+local function vec3_dot(ax, ay, az, bx, by, bz)
+	return ax*bx + ay*by + az*bz
 end
 
-local function var_0_15(arg_3_0, arg_3_1)
-	local var_3_0 = var_0_10(arg_3_0)
-	local var_3_1 = var_0_10(arg_3_1)
-	local var_3_2 = var_0_3(var_3_0)
-	local var_3_3 = var_0_4(var_3_0)
-	local var_3_4 = var_0_3(var_3_1)
-
-	return var_3_3 * var_0_4(var_3_1), var_3_3 * var_3_4, -var_3_2
+local function angle_to_vec(pitch, yaw)
+	local p, y = deg2rad(pitch), deg2rad(yaw)
+	local sp, cp, sy, cy = sin(p), cos(p), sin(y), cos(y)
+	return cp*cy, cp*sy, -sp
 end
 
-local function var_0_16(arg_4_0, arg_4_1, arg_4_2, arg_4_3)
-	local var_4_0, var_4_1 = var_0_0(arg_4_0, "m_angEyeAngles")
-
-	if var_4_0 == nil then
+local function aiming_at_me(ent, lx, ly, lz)
+	local pitch, yaw = get_prop(ent, "m_angEyeAngles")
+	if pitch == nil then
 		return false
 	end
 
-	local var_4_2, var_4_3, var_4_4 = var_0_15(var_4_0, var_4_1)
-	local var_4_5, var_4_6, var_4_7 = var_0_0(arg_4_0, "m_vecOrigin")
-
-	if var_4_5 == nil then
+	local ex, ey, ez = angle_to_vec(pitch, yaw)
+	local px, py, pz = get_prop(ent, "m_vecOrigin")
+	if px == nil then
 		return false
 	end
 
-	local var_4_8, var_4_9, var_4_10 = var_0_13(arg_4_1 - var_4_5, arg_4_2 - var_4_6, arg_4_3 - var_4_7)
-
-	return var_0_14(var_4_8, var_4_9, var_4_10, var_4_2, var_4_3, var_4_4) > var_0_11
+	local dx, dy, dz = vec3_normalize(lx-px, ly-py, lz-pz)
+	return vec3_dot(dx, dy, dz, ex, ey, ez) > fov_cos
 end
 
-local function var_0_17()
-	local var_5_0 = var_0_5()
-
-	if var_5_0 == nil then
+local function check_threats()
+	local entindex = get_local_player()
+	if entindex == nil then
 		return false, nil
 	end
 
-	local var_5_1, var_5_2, var_5_3 = var_0_0(var_5_0, "m_vecOrigin")
-
-	if var_5_1 == nil then
+	local lx, ly, lz = get_prop(entindex, "m_vecOrigin")
+	if lx == nil then
 		return false, nil
 	end
 
-	local var_5_4 = var_0_6(true)
-
-	for iter_5_0 = 1, #var_5_4 do
-		local var_5_5 = var_5_4[iter_5_0]
-
-		if var_0_16(var_5_5, var_5_1, var_5_2, var_5_3) then
-			return true, var_0_7(var_5_5) or "An enemy"
+	local players = get_all_players(true)
+	for i=1, #players do
+		entindex = players[i]
+		if aiming_at_me(entindex, lx, ly, lz) then
+			return true, get_player_name(entindex) or "An enemy"
 		end
 	end
 
 	return false, nil
 end
 
-local function var_0_18()
-	local var_6_0, var_6_1 = var_0_17()
-
-	if var_6_0 then
-		local var_6_2, var_6_3 = var_0_8()
-
-		var_0_9(var_6_2 / 2, var_6_3 - 100, 255, 255, 50, 255, "c+", 0, var_6_1, " is aiming in your direction")
+local function on_paint()
+	local found, name = check_threats()
+	if found then
+		local sw, sh = get_screen_size()
+		text(sw / 2, sh - 200, 255, 255, 50, 255, "c+", 0, name, " is aiming in your direction")
 	end
 end
 
-local function var_0_19(arg_7_0)
-	if var_0_1(arg_7_0) then
-		client.set_event_callback("paint", var_0_18)
+local function toggle_callback(item)
+	if ui_get(item) then
+		client.set_event_callback("paint", on_paint)
 	else
-		client.unset_event_callback("paint", var_0_18)
+		client.unset_event_callback("paint", on_paint)
 	end
 end
 
-ui.set_callback(var_0_12, var_0_19)
+ui.set_callback(show_threats, toggle_callback)
