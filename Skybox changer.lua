2eca5f7c13cec2af2685d2d53f1fd6a9a2765936
Esdeath <39652390+notesdeath@users.noreply.github.com>
2025-01-09 14:59:54 +0100

    Update Skybox changer.lua

--- Skybox changer.lua
+++ Skybox changer.lua
@@ -1,152 +1,127 @@
-﻿local var_0_0 = client.delay_call
-local var_0_1 = client.set_event_callback
-local var_0_2 = client.userid_to_entindex
-local var_0_3 = entity.get_local_player
-local var_0_4 = materialsystem.find_materials
-local var_0_5 = pairs
-local var_0_6 = table.insert
-local var_0_7 = table.sort
-local var_0_8 = ui.get
-local var_0_9 = ui.new_checkbox
-local var_0_10 = ui.new_color_picker
-local var_0_11 = ui.new_listbox
-local var_0_12 = ui.set_callback
-local var_0_13 = ui.set_visible
-local var_0_14 = type
-local var_0_15 = package.searchpath
+-- local variables for API functions. any changes to the line below will be lost on re-generation
+local client_delay_call, client_set_event_callback, client_userid_to_entindex, entity_get_local_player, materialsystem_find_materials, pairs, table_insert, table_sort, ui_get, ui_new_checkbox, ui_new_color_picker, ui_new_listbox, ui_set_callback, ui_set_visible, type = client.delay_call, client.set_event_callback, client.userid_to_entindex, entity.get_local_player, materialsystem.find_materials, pairs, table.insert, table.sort, ui.get, ui.new_checkbox, ui.new_color_picker, ui.new_listbox, ui.set_callback, ui.set_visible, type
 
-local function var_0_16(arg_1_0)
-	return var_0_15("", arg_1_0) == arg_1_0
+local package_searchpath = package.searchpath
+local function file_exists(filename)
+	return package_searchpath("", filename) == filename
 end
 
-local function var_0_17(arg_2_0)
-	return var_0_16("./csgo/materials/skybox/" .. arg_2_0 .. "up.vmt")
+local function skybox_exists(skybox_name)
+	return file_exists("./csgo/materials/skybox/" .. skybox_name .. "up.vmt")
 end
 
-local var_0_18 = cvar.sv_skyname
-local var_0_19 = {
-	["Custom: Ocean"] = "dreamyocean",
-	["Custom: Galaxy (2)"] = "clear_night_sky",
-	Italy = "italy",
-	["Custom: Galaxy"] = "sky_descent",
-	Monastery = "embassy",
-	["Custom: Sunrise"] = "amethyst",
-	Baggage = "cs_baggage_skybox_",
-	Rainy = "vietnam",
-	Dusty = "sky_dust",
-	["Night (Flat)"] = "sky_csgo_night_flat",
-	["Night (2)"] = "sky_csgo_night02b",
-	Night = "sky_csgo_night02",
-	["Clouds (Dark)"] = "sky_csgo_cloudy01",
-	Assault = "sky_cs15_daylight04_hdr",
-	Cobblestone = "sky_cs15_daylight03_hdr",
-	Canals = "sky_venice",
-	Clear = "nukeblank",
-	Gray = "sky_day02_05_hdr",
-	["Clouds (2)"] = "vertigo",
-	Clouds = "sky_cs15_daylight02_hdr",
-	["Daylight (2)"] = "vertigoblue_hdr",
-	Daylight = "sky_cs15_daylight01_hdr",
-	Vertigo = "office",
-	Aztec = "jungle",
-	["Custom: Night (Blue)"] = "sky561",
-	["Custom: Clouds"] = "sky091",
-	["Custom: Heaven (2)"] = "sky081",
-	["Custom: Heaven"] = "sky051",
-	["Custom: Grimm Night"] = "grimmnight",
-	Tibet = "cs_tibet",
-	["Custom: Clouds (Night)"] = "cloudynight",
-	["Custom: Galaxy (4)"] = "mpa112",
-	["Custom: Galaxy (3)"] = "otherworld"
+local sv_skyname = cvar.sv_skyname
+local skyboxes = {
+	["Tibet"]="cs_tibet",
+	["Baggage"]="cs_baggage_skybox_",
+	["Monastery"]="embassy",
+	["Italy"]="italy",
+	["Aztec"]="jungle",
+	["Vertigo"]="office",
+	["Daylight"]="sky_cs15_daylight01_hdr",
+	["Daylight (2)"]="vertigoblue_hdr",
+	["Clouds"]="sky_cs15_daylight02_hdr",
+	["Clouds (2)"]="vertigo",
+	["Gray"]="sky_day02_05_hdr",
+	["Clear"]="nukeblank",
+	["Canals"]="sky_venice",
+	["Cobblestone"]="sky_cs15_daylight03_hdr",
+	["Assault"]="sky_cs15_daylight04_hdr",
+	["Clouds (Dark)"]="sky_csgo_cloudy01",
+	["Night"]="sky_csgo_night02",
+	["Night (2)"]="sky_csgo_night02b",
+	["Night (Flat)"]="sky_csgo_night_flat",
+	["Dusty"]="sky_dust",
+	["Rainy"]="vietnam",
+	["Custom: Sunrise"]="amethyst",
+	["Custom: Galaxy"]="sky_descent",
+	["Custom: Galaxy (2)"]="clear_night_sky",
+	["Custom: Galaxy (3)"]="otherworld",
+	["Custom: Galaxy (4)"]="mpa112",
+	["Custom: Clouds (Night)"]="cloudynight",
+	["Custom: Ocean"]="dreamyocean",
+	["Custom: Grimm Night"]="grimmnight",
+	["Custom: Heaven"]="sky051",
+	["Custom: Heaven (2)"]="sky081",
+	["Custom: Clouds"]="sky091",
+	["Custom: Night (Blue)"]="sky561",
 }
-local var_0_20 = {}
 
-for iter_0_0, iter_0_1 in var_0_5(var_0_19) do
-	if iter_0_0:sub(0, 8) ~= "Custom: " or var_0_17(iter_0_1) then
-		var_0_6(var_0_20, iter_0_0)
+local skybox_names = {}
+for k,v in pairs(skyboxes) do
+	if k:sub(0, 8) ~= "Custom: " or skybox_exists(v) then
+	  table_insert(skybox_names, k)
 	end
 end
+table_sort(skybox_names)
 
-var_0_7(var_0_20)
+local enabled_reference = ui_new_checkbox("VISUALS", "Effects", "Override skybox")
+local color_reference = ui_new_color_picker("VISUALS", "Effects", "Override skybox", 255, 255, 255, 255)
+local skybox_reference = ui_new_listbox("VISUALS", "Effects", "Override skybox name", skybox_names)
 
-local var_0_21 = var_0_9("VISUALS", "Effects", "Override skybox")
-local var_0_22 = var_0_10("VISUALS", "Effects", "Override skybox", 255, 255, 255, 255)
-local var_0_23 = var_0_11("VISUALS", "Effects", "Override skybox name", var_0_20)
-local var_0_24 = false
-local var_0_25
+local enabled_prev, skyname_default = false, nil
 
-local function var_0_26()
-	local var_3_0 = var_0_8(var_0_21)
-
-	if var_3_0 or var_0_24 then
-		local var_3_1, var_3_2, var_3_3, var_3_4 = var_0_8(var_0_22)
-
-		if not var_3_0 then
-			var_3_1, var_3_2, var_3_3, var_3_4 = 255, 255, 255, 255
+local function on_color_changed()
+	local enabled = ui_get(enabled_reference)
+	if enabled or enabled_prev then
+		local r, g, b, a = ui_get(color_reference)
+		if not enabled then
+			r, g, b, a = 255, 255, 255, 255
 		end
-
-		local var_3_5 = var_0_4("skybox/")
-
-		for iter_3_0 = 1, #var_3_5 do
-			var_3_5[iter_3_0]:color_modulate(var_3_1, var_3_2, var_3_3)
-			var_3_5[iter_3_0]:alpha_modulate(var_3_4)
+		local materials = materialsystem_find_materials("skybox/")
+		for i=1, #materials do
+			materials[i]:color_modulate(r, g, b)
+			materials[i]:alpha_modulate(a)
 		end
 	end
 end
+ui_set_callback(color_reference, on_color_changed)
 
-var_0_12(var_0_22, var_0_26)
+local function on_skybox_changed()
+	local enabled = ui_get(enabled_reference)
 
-local function var_0_27()
-	local var_4_0 = var_0_8(var_0_21)
-
-	var_0_13(var_0_23, var_4_0)
-
-	if var_4_0 then
-		local var_4_1 = var_0_8(var_0_23)
-
-		if var_0_14(var_4_1) == "number" then
-			var_4_1 = var_0_20[var_4_1 + 1]
+	ui_set_visible(skybox_reference, enabled)
+	if enabled then
+		local skybox = ui_get(skybox_reference)
+		if type(skybox) == "number" then
+			skybox = skybox_names[skybox+1]
 		end
 
-		if var_0_25 == nil then
-			var_0_25 = var_0_18:get_string()
+		if skyname_default == nil then
+			skyname_default = sv_skyname:get_string()
 		end
 
-		var_0_18:set_string(var_0_19[var_4_1])
-
-		if #var_0_4("skybox/" .. var_4_1) == 0 then
-			var_0_0(0.2, var_0_26)
+		sv_skyname:set_string(skyboxes[skybox])
+		--check if new skybox is loaded, if not delay color update
+		if #(materialsystem_find_materials("skybox/" .. skybox)) == 0 then
+			client_delay_call(0.2, on_color_changed)
 		else
-			var_0_26()
+			on_color_changed()
 		end
-	elseif var_0_24 and var_0_25 ~= nil then
-		var_0_18:set_string(var_0_25)
+	elseif enabled_prev then
+		if skyname_default ~= nil then
+			sv_skyname:set_string(skyname_default)
+		end
 	end
-
-	var_0_26()
-
-	var_0_24 = var_4_0
+	on_color_changed()
+	enabled_prev = enabled
 end
+ui_set_callback(enabled_reference, on_skybox_changed)
+ui_set_callback(skybox_reference, on_skybox_changed)
+on_skybox_changed()
 
-var_0_12(var_0_21, var_0_27)
-var_0_12(var_0_23, var_0_27)
-var_0_27()
-
-local function var_0_28(arg_5_0)
-	if var_0_2(arg_5_0.userid) == var_0_3() then
-		var_0_25 = nil
-
-		var_0_27()
-		var_0_26()
+local function on_player_connect_full(e)
+	if client_userid_to_entindex(e.userid) == entity_get_local_player() then
+		skyname_default = nil
+		on_skybox_changed()
+		on_color_changed()
 	end
 end
+client_set_event_callback("player_connect_full", on_player_connect_full)
 
-var_0_1("player_connect_full", var_0_28)
-
-local function var_0_29()
-	if var_0_25 ~= nil and var_0_8(var_0_21) then
-		var_0_18:set_string(var_0_25)
+local function on_shutdown()
+	if skyname_default ~= nil and ui_get(enabled_reference) then
+		sv_skyname:set_string(skyname_default)
 	end
 end
-
-var_0_1("shutdown", var_0_29)
+client_set_event_callback("shutdown", on_shutdown)
